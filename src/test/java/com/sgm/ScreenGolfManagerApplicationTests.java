9b3a18054215c44582f4b9cc27dfb9380ea5145f
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-03-29 17:53:50 +0900

    초기 설정 세팅

---
+++ src/test/java/com/sgm/ScreenGolfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenGolfManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
